6975e8cbe6b1f67e2f0e50d866d20b6dd1a78ed6
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-06-15 12:17:18 +0600

    added cart schema

---
+++ schemas/cart.schema.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const cartSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+        },
+
+        items: [
+            {
+                product: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Product",
+                },
+
+                quantity: {
+                    type: Number,
+                    default: 1,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Cart = mongoose.models.Cart || mongoose.model("Cart", cartSchema)
+export default Cart
