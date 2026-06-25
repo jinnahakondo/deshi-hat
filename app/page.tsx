e441ccb90fbed3c9922ced3b8f50f2e4ef7c38da
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-06-25 10:59:10 +0600

    added suspense in home section for sowing skeleton for products

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,16 @@ import PopularSection from "@/components/home/PopularSection";
 import { cateWiseProducts } from "@/data/categoryWiseProduct";
 import CategoryWiseProducts from "@/components/home/category/CategoryWiseProducts";
 import { Suspense } from "react";
+import ProductCardSkeleton from "@/components/skeleton/ProductCardSekleton";
 
 
 export default async function Home() {
+
+  const loading = <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 pb-16 gap-6">
+    {
+      [...Array(4)].map((_, i) => <ProductCardSkeleton key={i} />)
+    }
+  </div>
 
   return (
     <div className="w-full pb-16 space-y-16">
@@ -15,26 +22,17 @@ export default async function Home() {
       {/* <Suspense fallback={'loading...'}> */}
       <CategorySection />
       {/* </Suspense> */}
-      {/* <Suspense fallback={'loading...'}> */}
+      <Suspense fallback={loading}>
       <PopularSection />
-      {/* </Suspense> */}
-      {/* {
-        cateWiseProducts.map(product => <Suspense key={product.categorySlug} fallback={'loading...'}>
+      </Suspense>
+      {
+        cateWiseProducts.map(product => <Suspense key={product.categorySlug} fallback={loading}>
           <CategoryWiseProducts
             key={product.categorySlug}
             categorySlug={product.categorySlug} >
             {product.title}
           </CategoryWiseProducts>
         </Suspense>)
-      } */}
-
-      {
-        cateWiseProducts.map(product => <CategoryWiseProducts
-          key={product.categorySlug}
-          categorySlug={product.categorySlug} >
-          {product.title}
-        </CategoryWiseProducts>
-        )
       }
 
     </div>
