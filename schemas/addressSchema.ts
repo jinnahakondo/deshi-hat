57c0a1412d120273f891ef29d4ce07c61dee78b8
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-07-23 20:13:58 +0600

    address schema update

--- schemas/addressSchema.ts
+++ schemas/addressSchema.ts
@@ -1,6 +1,11 @@
 import mongoose, { Schema } from "mongoose";
 
 const AddressSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
     name: String,
     phone: String,
     division: String,
@@ -8,7 +13,11 @@ const AddressSchema = new Schema({
     city: String,
     address: String,
     postalCode: String,
-})
+    isDefault: {
+        type: Boolean,
+        default: false,
+    }
+}, { timestamps: true })
 
 const Address = mongoose.models.Address || mongoose.model("Address", AddressSchema);
 export default Address;
